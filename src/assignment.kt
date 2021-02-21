1165facfd5fe22f629ba4da806b84cc85e4a191a
gerardine2001 <gerardinekaba@gmail.com>
2021-02-21 10:56:24 +0300

    asiignment

---
+++ src/assignment.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name ="gerardinekabatesi"
+    println(name)
+    var age =35
+    println(age)
+    var phone =250780030142
+    println(phone)
+    var weight =42
+    println(weight)
+    var citizen: Boolean= false
+    println(citizen)
+
+}
